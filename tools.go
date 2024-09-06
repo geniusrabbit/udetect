57eaf032be66f92d2f9a1f9b8a573af7d07013b5
Dmitry Ponomarev <demdxx@gmail.com>
2024-09-06 17:48:04 +0200

    Up dependencies

--- tools.go
+++ tools.go
@@ -6,7 +6,6 @@ package udetect
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "google.golang.org/genproto"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
